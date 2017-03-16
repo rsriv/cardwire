9a195b2f3e77b014ccf88c2f23861756fd3fc0fc
Rishi Srivastava <rishi.srivastava98@gmail.com>
2017-03-15 23:05:41 -0400

    Added global constants

---
+++ app/src/main/java/com/example/rishi/cardwire/Constants.java
@@ -0,0 +1,9 @@
+package com.example.rishi.cardwire;
+
+/**
+ * Created by rishi on 2017-03-15.
+ */
+
+public class Constants {
+    public static final String SERVER_URL = "http://39199f14.ngrok.io";
+}
